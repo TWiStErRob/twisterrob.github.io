5db64e8d565936644ffe25badbf1f76da30aeb1a
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-07-04 00:50:07 +0100

    Fix folder

--- .github/workflows/install-fontcustom.sh
+++ .github/workflows/install-fontcustom.sh
@@ -21,7 +21,7 @@ gem install fontcustom --version 2.0.0
 sudo apt-get install fontforge=1:20201107~dfsg-4build1
 
 git -c advice.detachedHead=false clone --branch v1.3.1 --depth 1 https://github.com/bramstein/sfnt2woff-zopfli.git \
-  && pushd sfnt2w off-zopfli \
+  && pushd sfnt2woff-zopfli \
   && make \
   && mv sfnt2woff-zopfli sfnt2woff \
   && echo "${PWD}" >> "${GITHUB_PATH}" \
