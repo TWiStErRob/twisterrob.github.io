3f4fbbd86907beffb5e48a8c2621a33c085bd751
Róbert Papp (TWiStErRob) <papp.robert.s@gmail.com>
2024-07-03 23:35:39 +0100

    Document

--- .github/workflows/install-fontcustom.sh
+++ .github/workflows/install-fontcustom.sh
@@ -1,14 +1,25 @@
 set -eo pipefail
+
 mkdir -p fontcustom && cd fontcustom
+
+# Workaround for
+# > Error installing fontcustom:
+# > The last version of ffi (~> 1.0) to support your Ruby & RubyGems was 1.17.0.
+# > Try installing it with `gem install ffi -v 1.17.0` and then running the current command again
+# > ffi requires RubyGems version >= 3.3.22. The current RubyGems version is 3.1.6.
+# > Try 'gem update --system' to update RubyGems itself.
 gem update --system
 gem install fontcustom --version 2.0.0
+
 sudo apt-get install fontforge=1:20201107~dfsg-4build1
+
 git -c advice.detachedHead=false clone --branch v1.3.1 --depth 1 https://github.com/bramstein/sfnt2woff-zopfli.git \
   && pushd sfnt2woff-zopfli \
   && make \
   && mv sfnt2woff-zopfli sfnt2woff \
   && echo "${PWD}" >> "${GITHUB_PATH}" \
   && popd
+
 git -c advice.detachedHead=false clone --branch v1.0.2 --depth 1 --recursive https://github.com/google/woff2.git \
   && pushd woff2 \
   && make clean all \
